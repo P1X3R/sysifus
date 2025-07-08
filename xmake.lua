8237660609970234ed5611f0232d37df0d2a2a66
p1x3r <165520679+P1X3R@users.noreply.github.com>
2025-07-08 18:08:17 -0500

    Update xmake.lua
    Compile tests conditionally

--- xmake.lua
+++ xmake.lua
@@ -9,11 +9,17 @@ target("sysifus")
   add_includedirs("include", { public = true })
   set_pcheader("include/luts.h")
 
-target("sysifusTesting")
-  set_kind("binary")
-  set_languages("c99")
-  set_warnings("all", "error")
-  add_files("test/main.c")
-  add_deps("sysifus")
-  add_includedirs("include")
-  add_links("check")
+option("compile_sysifus_tests")
+  set_default(false) -- Default to disabled
+  set_showmenu(true) -- Show in `xmake f --menu`
+
+target("sysifus_testing")
+  if has_config("compile_sysifus_tests") then
+    set_kind("binary")
+    set_languages("c99")
+    set_warnings("all", "error")
+    add_files("test/main.c")
+    add_deps("sysifus")
+    add_includedirs("include")
+    add_links("check")
+  end
